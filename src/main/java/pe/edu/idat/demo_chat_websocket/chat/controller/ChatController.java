7f9809d2b331d04d091b5cfed4d5afaed8efdbfc
Luis-Angel-Salvatierra-Aquino <d18159.github@gmail.com>
2025-03-11 08:35:53 -0500

    controller terminado

--- src/main/java/pe/edu/idat/demo_chat_websocket/chat/controller/ChatController.java
+++ src/main/java/pe/edu/idat/demo_chat_websocket/chat/controller/ChatController.java
@@ -1,4 +1,28 @@
 package pe.edu.idat.demo_chat_websocket.chat.controller;
 
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+import org.springframework.stereotype.Controller;
+import pe.edu.idat.demo_chat_websocket.chat.model.Mensaje;
+
+@Controller
 public class ChatController {
+
+    @MessageMapping("/chat.sendMessage")
+    @SendTo("/topic/public")
+    public Mensaje enviarMensaje(
+           @Payload Mensaje mensaje){
+        return mensaje;
+    }
+    @MessageMapping("/chat.addUser")
+    @SendTo("/topic/public")
+    public Mensaje agregarUsuario(Mensaje mensaje,
+                                  SimpMessageHeaderAccessor
+                                          headerAccessor){
+        headerAccessor.getSessionAttributes().put("username",
+                mensaje.getEnvio());
+        return mensaje;
+    }
 }
