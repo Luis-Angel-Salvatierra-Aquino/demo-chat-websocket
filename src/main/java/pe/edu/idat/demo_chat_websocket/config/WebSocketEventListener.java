3d8eb89caa676cf899412d626bf142c67542959f
Luis-Angel-Salvatierra-Aquino <d18159.github@gmail.com>
2025-03-11 08:23:40 -0500

    configuración terminada

--- src/main/java/pe/edu/idat/demo_chat_websocket/config/WebSocketEventListener.java
+++ src/main/java/pe/edu/idat/demo_chat_websocket/config/WebSocketEventListener.java
@@ -1,7 +1,12 @@
 package pe.edu.idat.demo_chat_websocket.config;
 
+import org.springframework.context.event.EventListener;
 import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.stereotype.Component;
+import org.springframework.web.socket.messaging.SessionDisconnectEvent;
+import pe.edu.idat.demo_chat_websocket.chat.model.Mensaje;
+import pe.edu.idat.demo_chat_websocket.chat.model.TipoMensaje;
 
 @Component
 public class WebSocketEventListener {
@@ -12,5 +17,21 @@ public class WebSocketEventListener {
         this.messageSendingOperations = messageSendingOperations;
     }
 
+    @EventListener
+    public void socketDisconectListener(
+            SessionDisconnectEvent event){
+        StompHeaderAccessor headerAccessor =
+                StompHeaderAccessor.wrap(event.getMessage());
+        String usuario = (String)headerAccessor
+                .getSessionAttributes().get("username");
+        if(usuario != null){
+            Mensaje mensaje = new Mensaje();
+            mensaje.setTipo(TipoMensaje.DEJAR);
+            mensaje.setEnvio(usuario);
+            messageSendingOperations.convertAndSend(
+                    "/topic/public", mensaje);
+        }
+
+    }
 
 }
