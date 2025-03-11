363fd9e65a09517a7d9b152fd703d00c710ab1e3
Luis-Angel-Salvatierra-Aquino <d18159.github@gmail.com>
2025-03-11 08:01:27 -0500

    configuracion de websocket

--- src/main/java/pe/edu/idat/demo_chat_websocket/chat/model/Mensaje.java
+++ src/main/java/pe/edu/idat/demo_chat_websocket/chat/model/Mensaje.java
@@ -1,4 +1,7 @@
 package pe.edu.idat.demo_chat_websocket.chat.model;
 
 public class Mensaje {
+    private TipoMensaje tipo;
+    private String contenido;
+    private String envio;
 }
--- src/main/java/pe/edu/idat/demo_chat_websocket/chat/model/TipoMensaje.java
+++ src/main/java/pe/edu/idat/demo_chat_websocket/chat/model/TipoMensaje.java
@@ -1,4 +1,5 @@
 package pe.edu.idat.demo_chat_websocket.chat.model;
 
 public enum TipoMensaje {
+    CHAT, UNIR, DEJAR
 }
