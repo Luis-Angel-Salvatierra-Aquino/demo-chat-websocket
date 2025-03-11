3389ff23d4d6b917ad6ae98330ae8bef72f2db74
Luis-Angel-Salvatierra-Aquino <d18159.github@gmail.com>
2025-03-11 08:11:16 -0500

    event web socket

--- src/main/java/pe/edu/idat/demo_chat_websocket/config/WebSocketEventListener.java
+++ src/main/java/pe/edu/idat/demo_chat_websocket/config/WebSocketEventListener.java
@@ -1,4 +1,16 @@
 package pe.edu.idat.demo_chat_websocket.config;
 
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.stereotype.Component;
+
+@Component
 public class WebSocketEventListener {
+
+    private final SimpMessageSendingOperations messageSendingOperations;
+
+    public WebSocketEventListener(SimpMessageSendingOperations messageSendingOperations) {
+        this.messageSendingOperations = messageSendingOperations;
+    }
+
+
 }
