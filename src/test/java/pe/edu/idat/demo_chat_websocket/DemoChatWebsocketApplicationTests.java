026d1effcf746d65ec9e62c73195cb615dd92496
Luis-Angel-Salvatierra-Aquino <d18159.github@gmail.com>
2025-03-11 07:32:57 -0500

    primer commit

---
+++ src/test/java/pe/edu/idat/demo_chat_websocket/DemoChatWebsocketApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.idat.demo_chat_websocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoChatWebsocketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
