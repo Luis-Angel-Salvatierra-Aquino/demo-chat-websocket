6ddcf77458b814b50e461e4266dcb04159e1acf4
Luis-Angel-Salvatierra-Aquino <d18159.github@gmail.com>
2025-03-11 07:41:47 -0500

    clases del proyecto

---
+++ src/main/java/pe/edu/idat/demo_chat_websocket/chat/controller/ChatController.java
@@ -0,0 +1,4 @@
+package pe.edu.idat.demo_chat_websocket.chat.controller;
+
+public class ChatController {
+}
---
+++ src/main/java/pe/edu/idat/demo_chat_websocket/config/WebSocketConfig.java
@@ -0,0 +1,4 @@
+package pe.edu.idat.demo_chat_websocket.config;
+
+public class WebSocketConfig {
+}
